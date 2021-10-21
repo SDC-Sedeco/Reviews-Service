cb10ff3899481af2979a1cc055f9cd3560bc22fd
Logan Michalicek <logan.michalicek@gmail.com>
2021-10-20 21:43:06 -0600

    Confirmed database update via server

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,22 @@ var models = require('../models/index.js');
 
 module.exports = {
   reviews: {
-    get: function() {},
-    post: function() {},
+    get: function(req, res) {
+      res.statusCode = '201';
+      res.end();
+    },
+    post: function(req, res) {
+      models.reviews.post()
+        .then(() => {
+          res.statusCode = '201';
+          res.end();
+        })
+        .catch((err) => {
+          console.log(err);
+          res.statusCode = '401';
+          res.end();
+        })
+    },
     helpful: function() {},
     report: function() {}
   },
