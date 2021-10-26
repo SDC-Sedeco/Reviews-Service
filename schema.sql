fcc67f639fc95a0e92c49b43219e9cf7ea6da9bc
Logan Michalicek <logan.michalicek@gmail.com>
2021-10-26 07:42:58 -0600

    Updated API endpoints to remove test values

--- schema.sql
+++ schema.sql
@@ -6,15 +6,15 @@ CREATE TABLE reviews (
   id INT PRIMARY KEY AUTO_INCREMENT,
   product_id INT NOT NULL,
   rating INT NOT NULL,
-  summary TEXT NOT NULL,
-  recommend BOOLEAN NOT NULL DEFAULT FALSE,
-  response TEXT DEFAULT NULL,
-  body TEXT NOT NULL,
   date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  summary TEXT NOT NULL,
+  body TEXT NOT NULL,
+  recommend BOOLEAN NOT NULL DEFAULT FALSE,
+  reported BOOLEAN NOT NULL DEFAULT FALSE,
   reviewer_name TEXT NOT NULL,
-  email TEXT NOT NULL,
-  helpfulness INT NOT NULL DEFAULT 0,
-  reported BOOLEAN NOT NULL DEFAULT FALSE
+  reviewer_email TEXT NOT NULL,
+  response TEXT DEFAULT NULL,
+  helpfulness INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE characteristics (
--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = 3000;
 app.use(morgan('dev'));
 app.use(parser.json());
 
-app.use('/test', router);
+app.use('/reviews', router);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
