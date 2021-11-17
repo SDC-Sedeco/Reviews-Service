9871899131aa020ab073cb2c2452a995d8113e76
Logan Michalicek <logan.michalicek@gmail.com>
2021-11-16 20:21:19 -0700

    Fixed routing issue

--- routes.js
+++ routes.js
@@ -3,9 +3,6 @@ var router = require('express').Router();
 
 router.get('/', controller.reviews.get);
 router.get('/meta', controller.meta.get);
-router.get('/loaderio*', (req, res) => {
-  res.status(200).send('loaderio-53bd3fe83f6e107449f0c80c9da4d8d1');
-});
 
 router.post('/', controller.reviews.post);
 router.put('/:review_id/helpful', controller.reviews.helpful);
--- server/index.js
+++ server/index.js
@@ -11,4 +11,8 @@ app.use(parser.json());
 
 app.use('/reviews', router);
 
+app.get('/loaderio*', (req, res) => {
+  res.status(200).send('loaderio-53bd3fe83f6e107449f0c80c9da4d8d1');
+});
+
 module.exports = app;
